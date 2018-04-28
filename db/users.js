e41ef02d6b95564358c85d07bf191950f20a0621
nic fontaine <ngpfontaine@protonmail.com>
2018-04-27 17:03:32 -0700

    working, currently set to insecure

---
+++ db/users.js
@@ -0,0 +1,37 @@
+// TESTING
+var records = [
+  {
+    id: 1,
+    username: 'username',
+    password: "password",
+    admin: false
+  },
+  {
+    id: 2,
+    username: 'admin',
+    password: "password",
+    admin: true
+  },
+]
+exports.findById = function(id, cb) {
+  process.nextTick(function() {
+    var idx = id - 1
+    if (records[idx]) {
+      cb(null, records[idx])
+    } else {
+      cb(new Error('User ' + id + ' does not exist'))
+    }
+  })
+}
+
+exports.findByUsername = function(username, cb) {
+  process.nextTick(function() {
+    for (var i = 0, len = records.length; i < len; i++) {
+      var record = records[i]
+      if (record.username === username) {
+        return cb(null, record)
+      }
+    }
+    return cb(null, null)
+  })
+}
---
+++ sesh.js
@@ -0,0 +1,105 @@
+/*
+
+/ Login form
+POST to /login
+Authenticate with Passport
+GET /success or /fail
+GET to /success checks for cookie
+If no or invalid cookie, redirect to /cookie-fail
+
+*/
+
+"use strict"
+const express = require("express"),
+  session = require("express-session"),
+  FileStore = require("session-file-store")(session),
+  connect = require("connect-ensure-login"),
+  passport = require("passport"),
+  Strategy = require("passport-local").Strategy,
+  app = express(),
+  port = 3340,
+  nanoid = require('nanoid'),
+  db = require('./db')
+
+  passport.serializeUser(function(user, cb) {
+    cb(null, user.id)
+  })
+  passport.deserializeUser(function(id, cb) {
+    db.users.findById(id, function (err, user) {
+      if (err) { return cb(err) }
+      cb(null, user)
+    })
+  })
+  passport.use(new Strategy(
+    function(username, password, cb) {
+      db.users.findByUsername(username, function(err, user) {
+        if (err) {return cb(err) }
+        if (!user) { return cb(null, false) }
+        if (user.password != password) { return cb(null, false) }
+        return cb(null, user) // Sucess
+      })
+    }
+  ))
+
+app.use(require('body-parser').urlencoded({ extended: true }))
+
+// app.set("trust proxy", 1)
+app.use(session({
+  name: "name-" + nanoid(6),
+  secret: "secret-" + nanoid(6),
+  saveUninitialized: false,
+  // resave: true,
+  store: new FileStore(),
+  // proxy: true,
+  domain: "node.nicfontaine.com",
+  cookie: {
+    // secure: true,
+    // httpOnly: true
+  }
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Send pre-populated login form
+app.get("/", function(req,res) {
+  res.send("<html><head><meta charset='UTF-8'></head><body>" + 
+    "<form action='/login' method='post'>" +
+      "<input name='username' value='username'><br>" +
+      "<input name='password' value='password'><br>" +
+      "<input type='submit' value='LOGIN'>" +
+    "</form>" +
+  "</body>" +
+  "</html>")
+})
+
+// Authenticate credentials
+app.post("/login",
+  passport.authenticate('local', {
+    successRedirect: "/success",
+    failureRedirect: "/fail"
+  })
+)
+
+// Authentication required to view
+app.get("/success",
+  connect.ensureLoggedIn("/cookie-fail"),
+  function(req,res) {
+    res.setHeader("Content-Type", "text/html")
+    res.send("Authentication Success<br>" +
+      "<a href='./'>Back</a>") })
+
+// Auth failed
+app.get("/fail",
+  function(req,res) { res.send("Authentication Fail<br>" +
+    "<a href='./'>Back</a>") })
+
+// Cookie auth failed
+app.get("/cookie-fail",
+  function(req,res) { res.send("Authentication Success, But Cookie Authentication Fail<br>" +
+    "<a href='./'>Back</a>") })
+
+app.listen(port, (err) => {
+  if (err) { return console.log('node: something bad happened', err) }
+  console.log('node: server is listening on ' + port)
+})
